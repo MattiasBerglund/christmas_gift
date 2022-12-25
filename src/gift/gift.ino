0dd1f4f3648b352e5e9a97b3d57b6e7cedc6ff15
Mattias Berglund <berglundmattias95@gmail.com>
2022-12-25 17:01:23 +0100

    Christmas day: Some trimming with Henke

--- src/gift/gift.ino
+++ src/gift/gift.ino
@@ -1,32 +1,55 @@
 #include <NewPing.h>
 
-#define PIN_SENSOR_0  12  
-#define LED_SENSOR_0  10
+#define PIN_SENSOR_0  2  
+#define LED_SENSOR_0  3
 
-#define PIN_SENSOR_1  3  
-#define LED_SENSOR_1  2
+#define PIN_SENSOR_1  4  
+#define LED_SENSOR_1  5
+
+#define PIN_SENSOR_2  6  
+#define LED_SENSOR_2  7
+
+#define PIN_SENSOR_3  8  
+#define LED_SENSOR_3  9
+
+#define PIN_SENSOR_4  10  
+#define LED_SENSOR_4  11
+
 
 #define MAX_DISTANCE  200
-#define SONAR_NUM     2
+#define SONAR_NUM     5
 
+// User Settings
 int min_dist = 2;
 int max_dist = 20;
+int rounds = 10;
+int min_sleep = 0;
+int max_sleep = 0;
+
+// Variables
+int played_rounds = 1;
 int default_sensor = 999;
 int active_sensor = default_sensor;
 int next_sensor = default_sensor;
-int min_sleep = 2000;
-int max_sleep = 5000;
+
 
 NewPing sonar[SONAR_NUM] = {   // Sensor object array.
   NewPing(PIN_SENSOR_0, PIN_SENSOR_0, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping. 
   NewPing(PIN_SENSOR_1, PIN_SENSOR_1, MAX_DISTANCE), 
+  NewPing(PIN_SENSOR_2, PIN_SENSOR_2, MAX_DISTANCE), 
+  NewPing(PIN_SENSOR_3, PIN_SENSOR_3, MAX_DISTANCE), 
+  NewPing(PIN_SENSOR_4, PIN_SENSOR_4, MAX_DISTANCE), 
 };
 
 void setup() {
   pinMode(LED_SENSOR_0, OUTPUT);
   pinMode(LED_SENSOR_1, OUTPUT);
+  pinMode(LED_SENSOR_2, OUTPUT);
+  pinMode(LED_SENSOR_3, OUTPUT);
+  pinMode(LED_SENSOR_4, OUTPUT);
+
   Serial.begin(115200);
-  startRandomLed();
+  startSession();
 }
 
 void loop() {
@@ -39,42 +62,145 @@ void loop() {
   }
 }
 
-void checkContact(int dist, int sensor){
+void startSession(){
+  played_rounds = 1;
+  startLedSensorStart();
+  
+  while (checkDist(sonar[2].ping_cm()) == false || checkDist(sonar[3].ping_cm()) == false){
+  }
+
+  startAllLeds();
+  delay(1000);
+  turnOffLeds();
+  delay(1000);
+  startAllLeds();
+  delay(1000);
+  turnOffLeds();
+  delay(1000);
+  startAllLeds();
+  delay(1000);
+  turnOffLeds();
+  delay(1000);
+  startRandomLed();
+}
+
+void stopSession(){
+  startAllLeds();
+  delay(3000);
+  startSession();
+}
+
+bool checkDist(int dist){
   if (dist >= min_dist && dist <= max_dist){
-    printDist(dist);
-    turnOffLeds();
-    delay(random(min_sleep, max_sleep));
-    startRandomLed();
+    return true;
+  }
+
+  return false;
+}
+void checkContact(int dist, int sensor){
+  if (checkDist(dist)){
+    if (played_rounds == rounds){
+      stopSession();
+    }
+    else {
+      printDist(dist);
+      turnOffLeds();
+      delay(random(min_sleep, max_sleep));
+      played_rounds = played_rounds + 1;
+      startRandomLed();
+    }    
   }
 }
 
 void startLedSensor0(){
   digitalWrite(LED_SENSOR_0, HIGH);
   digitalWrite(LED_SENSOR_1, LOW);
+  digitalWrite(LED_SENSOR_2, LOW);
+  digitalWrite(LED_SENSOR_3, LOW);
+  digitalWrite(LED_SENSOR_4, LOW);
 }
 
 void startLedSensor1(){
   digitalWrite(LED_SENSOR_0, LOW);
   digitalWrite(LED_SENSOR_1, HIGH);
+  digitalWrite(LED_SENSOR_2, LOW);
+  digitalWrite(LED_SENSOR_3, LOW);
+  digitalWrite(LED_SENSOR_4, LOW);
+}
+
+void startLedSensor2(){
+  digitalWrite(LED_SENSOR_0, LOW);
+  digitalWrite(LED_SENSOR_1, LOW);
+  digitalWrite(LED_SENSOR_2, HIGH);
+  digitalWrite(LED_SENSOR_3, LOW);
+  digitalWrite(LED_SENSOR_4, LOW);
+}
+
+void startLedSensor3(){
+  digitalWrite(LED_SENSOR_0, LOW);
+  digitalWrite(LED_SENSOR_1, LOW);
+  digitalWrite(LED_SENSOR_2, LOW);
+  digitalWrite(LED_SENSOR_3, HIGH);
+  digitalWrite(LED_SENSOR_4, LOW);
+}
+
+void startLedSensor4(){
+  digitalWrite(LED_SENSOR_0, LOW);
+  digitalWrite(LED_SENSOR_1, LOW);
+  digitalWrite(LED_SENSOR_2, LOW);
+  digitalWrite(LED_SENSOR_3, LOW);
+  digitalWrite(LED_SENSOR_4, HIGH);
+}
+
+void startLedSensorStart(){
+  digitalWrite(LED_SENSOR_0, LOW);
+  digitalWrite(LED_SENSOR_1, LOW);
+  digitalWrite(LED_SENSOR_2, HIGH);
+  digitalWrite(LED_SENSOR_3, HIGH);
+  digitalWrite(LED_SENSOR_4, LOW);
+}
+
+void startAllLeds(){
+  digitalWrite(LED_SENSOR_0, HIGH);
+  digitalWrite(LED_SENSOR_1, HIGH);
+  digitalWrite(LED_SENSOR_2, HIGH);
+  digitalWrite(LED_SENSOR_3, HIGH);
+  digitalWrite(LED_SENSOR_4, HIGH);
 }
 
 void startRandomLed(){
-  next_sensor = random(SONAR_NUM);
+  while (next_sensor == active_sensor){
+    next_sensor = random(SONAR_NUM);
+  }
+  
   Serial.print("Random number: ");
-  Serial.print(next_sensor);
+  Serial.println(next_sensor);
   if (next_sensor == 0){
       startLedSensor0();
     }
   else if (next_sensor == 1){
       startLedSensor1();
     }
+  else if (next_sensor == 2){
+      startLedSensor2();
+    }
+  else if (next_sensor == 3){
+      startLedSensor3();
+    }
+  else if (next_sensor == 4){
+      startLedSensor4();
+    }
   active_sensor = next_sensor;
-  next_sensor = default_sensor;
+  // next_sensor = default_sensor;
 }
 
 void turnOffLeds(){
   digitalWrite(LED_SENSOR_0, LOW);
   digitalWrite(LED_SENSOR_1, LOW);
+  digitalWrite(LED_SENSOR_2, LOW);
+  digitalWrite(LED_SENSOR_3, LOW);
+  digitalWrite(LED_SENSOR_4, LOW);
+
 }
 
 void printDist(int dist){
@@ -82,11 +208,3 @@ void printDist(int dist){
   Serial.print(dist);
   Serial.println("cm");
 }
-
-// TODO
-// [X] input from sensor
-// [X] make led light
-// [x] Toggle led with two sensors
-// [X] light up one led, turn off with sensor
-// [x] light up random led, turn off with sensor
-// [x] light up random led within a random time, turn of with sensor
